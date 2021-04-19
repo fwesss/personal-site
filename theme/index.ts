3fa1acb13aa2cbaa3c6d21f30fe93e96476dbe64
Westley Feller <feller.westley@gmail.com>
2021-04-19 15:59:24 -0700

    chore(styling): :art: setup Chakra UI and basic color scheme
    re #4

---
+++ theme/index.ts
@@ -0,0 +1,9 @@
+// theme.js
+import { extendTheme } from "@chakra-ui/react"
+// Global style overrides
+import styles from "./styles"
+
+const overrides = {
+	...styles,
+}
+export default extendTheme(overrides)
