6afcfcaee54108611890e5f90afd4f4eabf3d041
Westley Feller <feller.westley@gmail.com>
2021-04-30 23:14:12 -0700

    feat: :lipstick: add top/bottom padding for app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,40 +18,43 @@ const MyApp: FC<AppPropsErr> = ({ Component, pageProps, err }) => {
 	return (
 		<ChakraProvider theme={theme}>
 			<AnimateSharedLayout>
-				<Box
-					as="header"
-					h="16"
-					w="100%"
-					position="sticky"
-					top={0}
-					zIndex={1000}
-					pointerEvents="none"
-				>
+				<Box pb={50}>
 					<Box
-						height="100%"
-						mx="auto"
-						ps={{ base: "6", md: "8" }}
-						pe={{ base: "5", md: "4" }}
+						as="header"
+						h="16"
+						w="100%"
+						position="sticky"
+						top={0}
+						zIndex={1000}
+						pointerEvents="none"
+						background="transparent"
 					>
-						<Flex
-							as="nav"
-							aria-label="Site navigation"
-							align="center"
-							justify="space-between"
+						<Box
 							height="100%"
-							fontFamily="mono"
-							fontSize={{ base: "md", sm: "lg", md: "xl" }}
+							mx="auto"
+							ps={{ base: "6", md: "8" }}
+							pe={{ base: "5", md: "4" }}
 						>
-							<NextLink href="/" passHref>
-								<NavLink.Desktop h={10}>WF</NavLink.Desktop>
-							</NextLink>
-							<NavContent.Desktop display={{ base: "none", md: "flex" }} />
-							<NavContent.Mobile display={{ base: "flex", md: "none" }} />
-						</Flex>
+							<Flex
+								as="nav"
+								aria-label="Site navigation"
+								align="center"
+								justify="space-between"
+								height="100%"
+								fontFamily="mono"
+								fontSize={{ base: "md", sm: "lg", md: "xl" }}
+							>
+								<NextLink href="/" passHref>
+									<NavLink.Desktop h={10}>WF</NavLink.Desktop>
+								</NextLink>
+								<NavContent.Desktop display={{ base: "none", md: "flex" }} />
+								<NavContent.Mobile display={{ base: "flex", md: "none" }} />
+							</Flex>
+						</Box>
 					</Box>
-				</Box>
 
-				<Component {...pageProps} err={err} />
+					<Component {...pageProps} err={err} />
+				</Box>
 			</AnimateSharedLayout>
 		</ChakraProvider>
 	)
