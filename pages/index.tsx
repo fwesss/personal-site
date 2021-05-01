62f92e34cf86992409315e09a42d3bfb67ded66c
Westley Feller <feller.westley@gmail.com>
2021-04-30 23:12:22 -0700

    refactor: :recycle: move home into box and seperated container component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import { Box, Flex, Heading, Text } from "@chakra-ui/react"
 import React from "react"
 import sanity from "../utils/sanity-client"
 import { Project as ProjectT } from "../studio/schema"
-import { FadeContainer } from "../components/MotionContainer"
 import { Projects } from "../components/Projects/Projects"
+import { FadeBox } from "../components/MotionComponents"
 
 type UnwrapPromise<T> = T extends Promise<infer U> ? U : T
 type ProjectProps = UnwrapPromise<ReturnType<typeof getStaticProps>>["props"]
@@ -18,59 +18,57 @@ const Home: Home = ({ projects }) => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<FadeContainer as="main" maxW="100vw">
-				<main>
-					<Flex
-						h="calc(100vh - 4rem)"
-						align="center"
-						bottom="4rem"
-						position="relative"
-						ps={{ base: "2", md: "24", lg: "32", xl: "48" }}
-						pe={{ base: "7", md: "6", lg: "12", xl: "24" }}
-					>
-						<Box>
-							<Text
-								mb={0}
-								fontSize={{
-									base: "lg",
-									sm: "xl",
-									md: "2xl",
-									lg: "3xl",
-									xl: "4xl",
-								}}
-							>
-								Hi, I&apos;m
-							</Text>
-							<Heading
-								as="h1"
-								fontSize={{ base: "12vw", md: "10vw", lg: "9vw", xl: "8vw" }}
-							>
-								Wes Feller
-							</Heading>
-							<Text
-								width={{
-									base: "clamp(30ch, 100%, 40ch)",
-									sm: "clamp(45ch, 100%, 75ch)",
-								}}
-								lineHeight="1.35"
-								fontSize={{
-									base: "md",
-									sm: "lg",
-									md: "xl",
-									lg: "2xl",
-									xl: "3xl",
-								}}
-							>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-								eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet
-								mauris commodo quis imperdiet massa.
-							</Text>
-						</Box>
-					</Flex>
+			<FadeBox as="main">
+				<Flex
+					h="calc(100vh - 4rem)"
+					align="center"
+					bottom="4rem"
+					position="relative"
+					ps={{ base: "2", md: "24", lg: "32", xl: "48" }}
+					pe={{ base: "7", md: "6", lg: "12", xl: "24" }}
+				>
+					<Box>
+						<Text
+							mb={0}
+							fontSize={{
+								base: "lg",
+								sm: "xl",
+								md: "2xl",
+								lg: "3xl",
+								xl: "4xl",
+							}}
+						>
+							Hi, I&apos;m
+						</Text>
+						<Heading
+							as="h1"
+							fontSize={{ base: "12vw", md: "10vw", lg: "9vw", xl: "8vw" }}
+						>
+							Wes Feller
+						</Heading>
+						<Text
+							width={{
+								base: "clamp(30ch, 100%, 40ch)",
+								sm: "clamp(45ch, 100%, 75ch)",
+							}}
+							lineHeight="1.35"
+							fontSize={{
+								base: "md",
+								sm: "lg",
+								md: "xl",
+								lg: "2xl",
+								xl: "3xl",
+							}}
+						>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet
+							mauris commodo quis imperdiet massa.
+						</Text>
+					</Box>
+				</Flex>
 
-					<Projects projects={projects} />
-				</main>
-			</FadeContainer>
+				<Projects projects={projects} />
+			</FadeBox>
 		</>
 	)
 }
