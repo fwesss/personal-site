9ea80e7d484e363843d19f54c0ff8278a4ccf40e
Westley Feller <feller.westley@gmail.com>
2021-04-30 23:13:25 -0700

    feat: :lipstick: using Inter font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,7 +27,7 @@ class MyDocument extends Document {
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 
 					<link
-						href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+						href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;400&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
 						rel="stylesheet"
 					/>
 				</Head>
