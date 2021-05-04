e4f761e1cca74d818871e854a1d8e4062cb018a2
Westley Feller <feller.westley@gmail.com>
2021-05-04 09:43:17 -0700

    fix: :lipstick: faster page transition

--- components/MotionComponents.tsx
+++ components/MotionComponents.tsx
@@ -18,7 +18,7 @@ export const FadeContainer: FC<MotionContainerProps> = ({
 		exit={{ opacity: 0 }}
 		initial={{ opacity: 0 }}
 		animate={{ opacity: 1 }}
-		transition={{ delay: 0.2 }}
+		transition={{ duration: 0.175 }}
 		{...rest}
 	>
 		{children}
@@ -33,7 +33,7 @@ export const FadeBox: FC<MotionBoxProps> = ({ children, ...rest }) => (
 		exit={{ opacity: 0 }}
 		initial={{ opacity: 0 }}
 		animate={{ opacity: 1 }}
-		transition={{ delay: 0.2 }}
+		transition={{ duration: 0.175 }}
 		{...rest}
 	>
 		{children}
