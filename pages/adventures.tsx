1b3b9b7cebb93a9117ae32718bd68c70c539bde8
Westley Feller <feller.westley@gmail.com>
2021-06-04 20:54:39 -0700

    feature: links for moving camera

--- pages/adventures.tsx
+++ pages/adventures.tsx
@@ -6,15 +6,13 @@ import {
   ListItem,
   Text,
   UnorderedList,
+  useColorModeValue,
 } from "@chakra-ui/react"
 import geoJson from "@mapbox/togeojson"
-import { easeCubic } from "d3"
 import type { FeatureCollection } from "geojson"
 import mapboxgl from "mapbox-gl"
-import type { FC, SetStateAction } from "react"
-import { useEffect, useRef, useState } from "react"
-import type { MapRef } from "react-map-gl"
-import ReactMapGL, { FlyToInterpolator, Layer, Source } from "react-map-gl"
+import type { FC } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 import { DOMParser } from "xmldom"
 
 import type { Adventure } from "../studio/schema"
@@ -43,19 +41,8 @@ type UnwrapPromise<T> = T extends Promise<infer U> ? U : T
 type AdventureProps = UnwrapPromise<ReturnType<typeof getStaticProps>>["props"]
 
 const Map: FC<AdventureProps> = ({ token, style, tracks, adventures }) => {
-  const mapRef = useRef<MapRef>(null)
-  interface ViewPort {
-    width: string
-    height: string
-    latitude: number
-    longitude: number
-    zoom: number
-    pitch: number
-    bearing: number
-    transitionDuration?: number
-    transitionInterpolator?: FlyToInterpolator
-    transitionEasing?: (time: number) => number
-  }
+  const mapContainerRef = useRef<HTMLDivElement>(null)
+  const [map, setMap] = useState<mapboxgl.Map>(null)
 
   type Zoom = number
   type Bearing = number
@@ -65,83 +52,215 @@ const Map: FC<AdventureProps> = ({ token, style, tracks, adventures }) => {
 
   const parseViewport = (
     view: string
-  ): [Zoom, Bearing, Pitch, Longitude, Latitude] => {
+  ): [Zoom, Bearing, Pitch, Latitude, Longitude] => {
     const parsed = view.split(" ")
-    const [zoom, bearing, pitch, longitude, latitude] = parsed.map(x =>
+    const [zoom, bearing, pitch, latitude, longitude] = parsed.map(x =>
       Number(x)
     )
-    return [zoom, bearing, pitch, longitude, latitude]
+    return [zoom, bearing, pitch, latitude, longitude]
   }
 
-  const [viewport, setViewport] = useState<ViewPort>({
-    width: "100%",
-    height: "100%",
-    latitude: 35.790266,
-    longitude: -118.063029,
-    zoom: 14.19,
-    pitch: 43.92,
-    bearing: 0,
-  })
+  const rotate = useCallback(() => {
+    map.rotateTo(map.getBearing() + 90, {
+      duration: 24000,
+      easing: t => t,
+    })
+  }, [map])
 
-  const updateView = ([zoom, bearing, pitch, longitude, latitude]: [
+  const rotateCamera = () => {
+    if (map) {
+      map.on("moveend", rotate)
+    }
+  }
+
+  const fly = ([zoom, bearing, pitch, longitude, latitude]: [
     Zoom,
     Bearing,
     Pitch,
     Longitude,
     Latitude
   ]) => {
-    setViewport({
-      ...viewport,
-      latitude,
-      longitude,
+    map.flyTo({
       zoom,
-      pitch,
       bearing,
-      transitionDuration: 5000,
-      transitionInterpolator: new FlyToInterpolator(),
-      transitionEasing: easeCubic,
-    })
-  }
-
-  const rotateCamera = () => {
-    const map = mapRef.current.getMap() as mapboxgl.Map
-    map.on("moveend", () => {
-      const rotateNumber = map.getBearing()
-      map.rotateTo(rotateNumber + 90, {
-        duration: 24000,
-        easing: t => t,
-      })
+      pitch,
+      center: [longitude, latitude],
     })
   }
 
   useEffect(() => {
-    rotateCamera()
-  }, [])
+    const initMap = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style,
+      accessToken: token,
+      center: [-118.063029, 35.790266],
+      zoom: 14.19,
+      pitch: 43.92,
+      bearing: 0,
+    })
 
-  const [activeAdventure, setActiveAdventure] = useState(adventures[0])
+    initMap.on("load", () => {
+      tracks.forEach((track, i) => {
+        initMap.addSource(`line${i}`, {
+          data: {
+            ...track,
+            features: track.features.filter(feature =>
+              ["MultiLineString", "LineString"].includes(feature.geometry.type)
+            ),
+          },
+          type: "geojson",
+        })
+
+        initMap.addLayer({
+          id: `line${i}`,
+          source: `line${i}`,
+          type: "line",
+          paint: {
+            "line-color": theme.colors.cyan["600"],
+            "line-width": 2,
+          },
+        })
+
+        if (track.features.some(feature => feature.geometry.type === "Point")) {
+          initMap.addSource("points", {
+            type: "geojson",
+            data: {
+              ...track,
+              features: track.features.filter(
+                feature => feature.geometry.type === "Point"
+              ),
+            },
+          })
+
+          initMap.addLayer({
+            id: "points",
+            source: "points",
+            type: "symbol",
+            layout: { "icon-size": 2, "icon-image": "marker-11" },
+            paint: {
+              "icon-color": theme.colors.yellow["400"],
+              "text-color": theme.colors.cyan["100"],
+              "text-halo-color": theme.colors.cyan["700"],
+            },
+          })
+        }
+      })
+
+      setMap(initMap)
+    })
+
+    return () => initMap.remove()
+  }, [style, token, tracks])
+
+  useEffect(() => {
+    const stopRotation = () => {
+      map.off("moveend", rotate)
+      map.stop()
+    }
+    if (map) {
+      map.on("mousedown", stopRotation)
+    }
+  }, [rotate, map])
+
+  const [activeAdventure, setActiveAdventure] = useState({
+    ...adventures[0],
+    index: 0,
+  })
+
+  const goToAdventure = (
+    adventure: Adventure,
+    adventureIndex: number
+  ): void => {
+    fly(parseViewport(adventure.initialView))
+    setActiveAdventure({ ...adventure, index: adventureIndex })
+    rotateCamera()
+  }
+
+  useEffect(() => {
+    console.log(tracks[0])
+    console.log(
+      tracks[activeAdventure.index].features.filter(
+        feature =>
+          feature.geometry.type === "Point" &&
+          feature.properties.name.split(" ")[0] === "0"
+      )
+    )
+  }, [activeAdventure.index, tracks])
 
   return (
     <Box h="calc(100vh - 64px)">
-      <Container left={8} maxW="md" mt={8} position="absolute" zIndex={1000}>
+      <Container
+        _hover={{
+          bg: useColorModeValue(
+            theme.colors.gray["50"],
+            theme.colors.gray["900"]
+          ),
+        }}
+        bg={useColorModeValue(
+          `${theme.colors.gray["50"]}BB`,
+          `${theme.colors.gray["900"]}BB`
+        )}
+        left={8}
+        maxW="3xl"
+        mt={8}
+        position="absolute"
+        px={12}
+        zIndex={1000}
+      >
         <Heading as="h2">{activeAdventure.title}</Heading>
-        {activeAdventure.stories.map(story => (
+        {activeAdventure.stories.map((story, storyIndex) => (
           <div key={story._key}>
-            <Heading as="h3">{story.headline}</Heading>
+            <Button
+              as="h3"
+              fontSize={28}
+              size="lg"
+              variant="link"
+              onClick={() =>
+                fly(
+                  parseViewport(
+                    tracks[activeAdventure.index].features
+                      .filter(feature => feature.geometry.type === "Point")
+                      .filter(
+                        feature =>
+                          Number(feature.properties.name.split(" ")[0]) ===
+                          storyIndex
+                      )[0].properties.cmt
+                  )
+                )
+              }
+            >
+              {story.headline}
+            </Button>
             <Text>{story.body}</Text>
           </div>
         ))}
       </Container>
 
-      <Container maxW="xs" mt={8} position="absolute" right={8} zIndex={1000}>
+      <Container
+        _hover={{
+          bg: useColorModeValue(
+            theme.colors.gray["50"],
+            theme.colors.gray["900"]
+          ),
+        }}
+        bg={useColorModeValue(
+          `${theme.colors.gray["50"]}BB`,
+          `${theme.colors.gray["900"]}BB`
+        )}
+        maxW="xs"
+        mt={8}
+        position="absolute"
+        right={8}
+        zIndex={1000}
+      >
         <UnorderedList listStyleType="none" marginInlineStart={0}>
-          {adventures.map(adventure => (
+          {adventures.map((adventure, adventureIndex) => (
             <ListItem key={adventure._id} mb={2}>
               <Button
                 colorScheme="teal"
                 variant="ghost"
                 onClick={() => {
-                  updateView(parseViewport(adventure.initialView))
-                  setActiveAdventure(adventure)
+                  goToAdventure(adventure, adventureIndex)
                 }}
               >
                 {adventure.title}
@@ -150,68 +269,7 @@ const Map: FC<AdventureProps> = ({ token, style, tracks, adventures }) => {
           ))}
         </UnorderedList>
       </Container>
-      <ReactMapGL
-        {...viewport}
-        ref={mapRef}
-        mapboxApiAccessToken={token}
-        mapStyle={style}
-        onViewportChange={(nextViewport: SetStateAction<ViewPort>) =>
-          setViewport(nextViewport)
-        }
-      >
-        {tracks.map((track, i) => (
-          <div key={i}>
-            <Source
-              data={{
-                ...track,
-                features: track.features.filter(feature =>
-                  ["MultiLineString", "LineString"].includes(
-                    feature.geometry.type
-                  )
-                ),
-              }}
-              id={`line${i}`}
-              type="geojson"
-            >
-              <Layer
-                paint={{
-                  "line-color": theme.colors.cyan["600"],
-                  "line-width": 2,
-                }}
-                type="line"
-              />
-            </Source>
-
-            {track.features.some(
-              feature => feature.geometry.type === "Point"
-            ) && (
-              <Source
-                data={{
-                  ...track,
-                  features: track.features.filter(
-                    feature => feature.geometry.type === "Point"
-                  ),
-                }}
-                id="points"
-                type="geojson"
-              >
-                <Layer
-                  layout={{
-                    "icon-size": 2,
-                    "icon-image": "marker-11",
-                  }}
-                  paint={{
-                    "icon-color": theme.colors.yellow["400"],
-                    "text-color": theme.colors.cyan["100"],
-                    "text-halo-color": theme.colors.cyan["700"],
-                  }}
-                  type="symbol"
-                />
-              </Source>
-            )}
-          </div>
-        ))}
-      </ReactMapGL>
+      <Box ref={mapContainerRef} height="100%" width="100%" />
     </Box>
   )
 }
@@ -229,9 +287,9 @@ export const getStaticProps = async (): Promise<{
   const adventures = await sanity.getAll("adventure")
   const tracks = await Promise.all(
     adventures.map(async adventure => {
-      const expanded = ((await sanity.expand<Adventure>(
+      const expanded = (await sanity.expand<Adventure>(
         adventure.gpx.asset
-      )) as unknown) as SanityFileAsset
+      )) as unknown as SanityFileAsset
 
       const res = await fetch(expanded.url)
       const data = await res.blob()
