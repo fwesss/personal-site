068f7b954319b1bf01e871196f9c8de3e7dac851
Wes Feller <wes@fig.io>
2021-09-22 23:20:59 -0700

    Add commandbar

--- components/ToggleMotionButton.tsx
+++ components/ToggleMotionButton.tsx
@@ -1,13 +1,32 @@
 import { IconButton, useToast } from "@chakra-ui/react"
-import { useContext } from "react"
+import { useEffect, useContext } from "react"
 import { FaRegPauseCircle, FaPlayCircle } from "react-icons/fa"
 
-import { MotionContext } from "../pages/_app"
+import { WindowContext, MotionContext } from "../pages/_app"
 
 const ToggleMotionButton = (): JSX.Element => {
   const { motionPref, update } = useContext(MotionContext)
+  const windowState = useContext(WindowContext)
   const SwitchIcon = motionPref ? FaRegPauseCircle : FaPlayCircle
   const toast = useToast()
+
+  const toggleMotion = () => {
+    update({ motionPref: !motionPref })
+    toast({
+      title: `Motion ${motionPref ? "disabled" : "enabled"}.`,
+      description: `All movement based animations on this site have been ${
+        motionPref ? "disabled" : "enabled"
+      }.`,
+      status: "info",
+      duration: 4000,
+    })
+  }
+
+  useEffect(() => {
+    if (windowState) {
+      window.CommandBar.addCallback("toggleMotion", toggleMotion)
+    }
+  }, [windowState, motionPref])
 
   return (
     <IconButton
@@ -17,15 +36,7 @@ const ToggleMotionButton = (): JSX.Element => {
       variant="ghost"
       isRound
       onClick={() => {
-        update({ motionPref: !motionPref })
-        toast({
-          title: `Motion ${motionPref ? "disabled" : "enabled"}.`,
-          description: `All movement based animations on this site have been ${
-            motionPref ? "disabled" : "enabled"
-          }.`,
-          status: "info",
-          duration: 4000,
-        })
+        toggleMotion()
       }}
     />
   )
